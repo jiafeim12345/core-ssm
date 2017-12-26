4f32a013f8eef4d61ea22eb9721ba221a986b8c9
zhenzhong.wang <zhenzhong.wang@hypers.com>
2017-12-26 15:58:36 +0800

    feat：新增excel处理工具

---
+++ src/test/java/me/cloudcat/develop/excel/DataClean.java
@@ -0,0 +1,20 @@
+package me.cloudcat.develop.excel;
+
+import me.cloudcat.develop.utils.ExcelUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * 清洗数据
+ * @Author: zhenzhong.wang
+ * @Time: 2017/12/25 14:22
+ */
+public class DataClean {
+
+    @Test
+    public void testOne() throws IOException, InvalidFormatException {
+        ExcelUtils.dataClean("C:\\Users\\ZZWang\\Desktop\\2017上半年上线.xlsx");
+    }
+}
