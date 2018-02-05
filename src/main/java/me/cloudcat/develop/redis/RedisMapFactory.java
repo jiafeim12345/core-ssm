3193bcee4d80e14ad63cfff3866770b8b5f699ce
zhenzhong.wang <zhenzhong.wang@hypers.com>
2018-02-05 19:12:02 +0800

    fix：修改redisTemplate注入方式

--- src/main/java/me/cloudcat/develop/redis/RedisMapFactory.java
+++ src/main/java/me/cloudcat/develop/redis/RedisMapFactory.java
@@ -1,20 +1,18 @@
 package me.cloudcat.develop.redis;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
 
 /**
  * @Author: zhenzhong.wang
  * @Time: 2018/2/5 16:07
  */
-@Component
 public class RedisMapFactory {
 
     private static RedisTemplate redisTemplate;
 
-    @Autowired
-    public void setRedisTemplate(RedisTemplate redisTemplate) {
+    public RedisMapFactory() {}
+
+    public RedisMapFactory(RedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
