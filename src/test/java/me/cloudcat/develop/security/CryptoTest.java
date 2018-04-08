5913bfa0f2dcf98a44fc05533a4441a30cde230b
zhenzhong.wang <jiafeim12345@163.com>
2018-04-08 19:10:02 +0800

    test：测试插入1W条数据mysql性能

--- src/test/java/me/cloudcat/develop/security/CryptoTest.java
+++ src/test/java/me/cloudcat/develop/security/CryptoTest.java
@@ -2,12 +2,16 @@ package me.cloudcat.develop.security;
 
 import me.cloudcat.develop.dao.UserDao;
 import me.cloudcat.develop.entity.User;
+import me.cloudcat.develop.entity.type.Status;
 import me.cloudcat.develop.utils.CryptoUtils;
+import org.apache.commons.lang.math.RandomUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.UUID;
 
 /**
  * @Author: zhenzhong.wang
@@ -28,6 +32,21 @@ public class CryptoTest {
         String encode = CryptoUtils.encode("Qiuqiu");
         user.setPassword(encode);
         userDao.updatePassword(user);
+    }
+
+    @Test
+    public void insertUser() {
+        int sum = 10000;
+        for (int i = 0; i < sum; i++) {
+            User user = new User();
+            user.setUsername(UUID.randomUUID().toString());
+            String encode = CryptoUtils.encode(UUID.randomUUID().toString());
+            user.setPassword(encode);
+            user.setEmail(UUID.randomUUID().toString());
+
+            user.setStatus(RandomUtils.nextBoolean() ? Status.ENABLE : Status.DISABLE);
+            userDao.save(user);
+        }
     }
 
 }
